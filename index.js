8860a6fd66605ba44813c280bbddb4213f424ed4
Quân vũ <quanva.b21cn618@stu.ptit.edu.vn>
2024-10-24 14:54:43 +0700

    24/10/2024 - second commit

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+const logger = require("./logger")
+const app = express()
+const port = process.env.PORT || 4000
 
-const token = process.env.TOKEN;
+app.use(bodyParser.json())
 
-async function getMeetings(){
-    try{
-        const response = await axios.get('https://api.zoom.us/v2/users/me/meetings',{
-            headers:{
-                'Authorization':`Bearer ${token}`
-            }
-        });
-        const data = response.data;
-        return data;
-    }catch(error){
-        console.error('Error',error);
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send(`Zoom Webhook sample successfully running. Set this URL with the /webhook path as your apps Event notification endpoint URL. https://github.com/zoom/webhook-sample`)
+})
+
+app.post('/webhook', (req, res) => {
+
+  var response
+  const zoomEvent = req.body;
+  logger.info('Zoom event received', {
+    headers: req.headers, 
+    body: zoomEvent,      
+  });
+
+
+  const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
+
+  const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex')
+
+  const signature = `v0=${hashForVerify}`
+
+ 
+  if (req.headers['x-zm-signature'] === signature) {
+
+    if(req.body.event === 'endpoint.url_validation') {
+      const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(req.body.payload.plainToken).digest('hex')
+
+      response = {
+        message: {
+          plainToken: req.body.payload.plainToken,
+          encryptedToken: hashForValidate
+        },
+        status: 200
+      }
+
+      console.log("message:::",response.message)
+
+      res.status(response.status)
+      res.json(response.message)
+    } else {
+      response = { message: 'Authorized request to Zoom Webhook sample.', status: 200 }
+      console.log("event:::",req.body.event)
+      console.log("message:::",response.message)
+
+      res.status(response.status)
+      res.json(response)
+
     }
-}
+  } else {
 
-async function createMeeting(topic, start_time,type,duration,timezone,agenda){
-    try{
-        const response = await axios.post('https://api.zoom.us/v2/users/me/meetings',{
-            topic,
-            type,
-            start_time,
-            duration,
-            timezone,
-            agenda,
-            settings:{
-                host_video:true,
-                participant_video:true,
-                join_before_host:false,
-                mute_upon_entry:true,
-                watermark:false,
-                use_pmi:false,
-                approval_type:0,
-                audio:'both',
-                auto_recording:'none'
-            }
-        },{
-            headers:{
-                'Authorization':`Bearer ${token}`
-            },
+    response = { message: 'Unauthorized request to Zoom Webhook sample.', status: 401 }
 
-        });
-        const body = response.data;
-        return body;
-    }catch(error){
-        console.error('Error',error);
-    }
-}
+    console.log("message::",response.message)
 
-(async()=>{
-    console.log(await getMeetings());
-    console.log(await createMeeting('CodingWithAdo new meeting','2023-11-20T10:00:00',2,45,'UTC','Team meeting for future videos'));
-    console.log(await getMeetings());
-})()
+    res.status(response.status)
+    res.json(response)
+  }
+  
+})
+
+app.listen(port, () => console.log(`Zoom Webhook sample listening on port ${port}!`))
